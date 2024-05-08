670913f08b6587dbe4cc02b35f35454b8b3be8ba
shahid <shahidhashmiraj@gmail.com>
2024-05-08 13:33:21 +0530

    completed frist project

--- First Project/app.js
+++ First Project/app.js
@@ -18,7 +18,7 @@ const scene = new THREE.Scene()
 //  Geometry
 
 const geometry = new THREE.BoxGeometry(1,1,1);
-const material = new THREE.MeshBasicMaterial({color: '#333'});
+const material = new THREE.MeshBasicMaterial({color: 'red', wireframe: true});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -28,6 +28,7 @@ const sizes = {
 };
 
 const camera = new THREE.PerspectiveCamera(75,sizes.width/ sizes.height);
+camera.position.z = 3
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
