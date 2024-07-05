42e1e01654b76af390c0b6cc4c5492de9e21f5ff
shahid <shahidhashmiraj@gmail.com>
2024-07-05 13:58:23 +0530

    3D text

---
+++ 12-text/src/script.js
@@ -0,0 +1,152 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {FontLoader} from 'three/examples/jsm/loaders/FontLoader.js'
+import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const mapcapTextures = textureLoader.load('/textures/matcaps/8.png')
+
+const Fonloader = new FontLoader();
+
+Fonloader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) => {
+       const textGeomerty = new TextGeometry(
+        'shahid hashmi',
+        {
+            font: font,
+            size: 0.5,
+            depth: 0.2,
+            curveSegments: 5,
+            bevelEnabled: true,
+            bevelThickness: 0.03,
+            bevelSize: 0.02,
+            bevelOffset: 0,
+            bevelSegments: 4
+        }
+       )
+       textGeomerty.computeBoundingBox();
+       console.log(textGeomerty.boundingBox);
+       textGeomerty.translate(
+        - (textGeomerty.boundingBox.max.x - 0.2) * 0.5,
+        - (textGeomerty.boundingBox.max.y - 0.2) * 0.5,
+        - (textGeomerty.boundingBox.max.y - 0.3) * 0.5,
+       );
+       const textMaterial = new THREE.MeshMatcapMaterial();
+       textMaterial.shadowSide = 10
+       textMaterial.matcap = mapcapTextures
+       mapcapTextures.minFilter = THREE.NearestFilter
+       mapcapTextures.magFilter = THREE.LinearFilter
+       const text = new THREE.Mesh(textGeomerty,textMaterial)
+       scene.add(text)
+        const torusGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+       for(let i = 0; i < 300; i++){
+        
+        const donut = new THREE.Mesh(torusGeometry,textMaterial);
+
+        donut.position.x = (Math.random() - 0.5) * 10
+        donut.position.y = (Math.random() - 0.5) * 10
+        donut.position.z = (Math.random() - 0.5) * 10
+
+        donut.rotation.x = Math.random() * Math.PI
+        donut.rotation.y = Math.random() * Math.PI
+
+        const scale = Math.random();
+        donut.scale.set(scale,scale,scale)
+
+        scene.add(donut)
+       }
+    }
+)
+/**
+ * Object
+ */
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
+
+// scene.add(cube)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
