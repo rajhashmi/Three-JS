835af4df4d02a1d7b65188d15220f81a1a5d35cd
shahid <shahidhashmiraj@gmail.com>
2024-07-24 23:28:43 +0530

    completed import models

--- 21-import models/src/script.js
+++ 21-import models/src/script.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import GUI from 'lil-gui'
+import { mix, threshold } from 'three/examples/jsm/nodes/Nodes.js'
 
 /**
  * Base
@@ -18,7 +21,7 @@ const scene = new THREE.Scene()
  * Floor
  */
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
+    new THREE.PlaneGeometry(100, 100),
     new THREE.MeshStandardMaterial({
         color: '#444444',
         metalness: 0,
@@ -28,6 +31,91 @@ const floor = new THREE.Mesh(
 floor.receiveShadow = true
 floor.rotation.x = - Math.PI * 0.5
 scene.add(floor)
+
+/**
+ * Models
+ * */ 
+
+/**
+ * animation for models
+ * 1. to add animation we need to add mixer make in global to we can update it in tick funcition
+ * */ 
+let mixer = null;
+
+
+// ==========
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/') // draco important
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader)
+gltfLoader.load(
+    '/models/lamp/street_lamp_01_1k.gltf',
+    (gltf) =>
+        {
+            console.log(gltf);
+            const model = gltf.scene;
+            // model.scale.set(0.025, 0.025, 0.025); // Adjust the scale factors to make the model smaller
+            model.position.x = 3
+            scene.add(model);
+
+
+            // mixer = new THREE.AnimationMixer(gltf.scene) // pasing object what we want to aniamtion and keep in mind that in tis object there is a propertty name animate where we'll find the animation
+
+            // window.addEventListener('keypress', (e)=>{
+            //     if(e.key === "w"){
+            //         const action = mixer.clipAction(gltf.animations[1]);
+            //         mixer.stopAllAction();
+        
+            //         action.reset().play();
+
+            //     }else if(e.key === "r"){
+            //         const action = mixer.clipAction(gltf.animations[2]);
+            //         mixer.stopAllAction();
+        
+            //         action.reset().play();
+            //     }
+            // })
+
+
+            
+        
+      
+        
+        }
+)
+
+// gltfLoader.load(
+//     '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+//     (gltf) =>
+//         {
+//             // const copyChildren = [...gltf.scene.children];
+//             // for(const child of copyChildren){
+//             //     scene.add(child)
+//             // }
+
+//             //  we can add scene as well 
+
+//             scene.add(gltf.scene)
+//         }
+// )
+
+
+// DRACOLoader
+// gltfLoader.load(
+//     '/models/Duck/glTF-Draco/Duck.gltf',
+//     (gltf) =>
+//         {
+//             // const copyChildren = [...gltf.scene.children];
+//             // for(const child of copyChildren){
+//             //     scene.add(child)
+//             // }
+
+//             //  we can add scene as well 
+
+//             scene.add(gltf.scene)
+//         }
+// )
+
 
 /**
  * Lights
@@ -104,6 +192,13 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
+
+    /***
+     * update mixture
+     * */ 
+    if(mixer !== null){
+        mixer.update(deltaTime)
+    }
 
     // Update controls
     controls.update()
