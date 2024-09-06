67b9f148b3ce5bddd6f5169eae7ea52ad344af9a
shahid <shahidhashmiraj@gmail.com>
2024-09-06 23:11:27 +0530

    removed lights

--- 47-intro-and-loader-progress/src/script.js
+++ 47-intro-and-loader-progress/src/script.js
@@ -129,13 +129,13 @@ gltfLoader.load(
 /**
  * Lights
  */
-const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
-directionalLight.castShadow = true
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.normalBias = 0.05
-directionalLight.position.set(0.25, 3, - 2.25)
-scene.add(directionalLight)
+// const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+// directionalLight.castShadow = true
+// directionalLight.shadow.camera.far = 15
+// directionalLight.shadow.mapSize.set(1024, 1024)
+// directionalLight.shadow.normalBias = 0.05
+// directionalLight.position.set(0.25, 3, - 2.25)
+// scene.add(directionalLight)
 
 /**
  * Sizes
