b51bc5c74491b67f6bb8c8f4ecfd8e22564dc16f
shahid <shahidhashmiraj@gmail.com>
2024-09-12 05:31:57 +0530

    completed react-fiber-first-project

--- 55-react-fiber-app/src/Experience.jsx
+++ 55-react-fiber-app/src/Experience.jsx
@@ -1,41 +1,55 @@
-import { useFrame, extend, useThree } from "@react-three/fiber"
-import { useRef } from "react"
+import { useFrame, extend, useThree } from "@react-three/fiber";
+import { useRef } from "react";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import CustomObject from "./CustomObject.jsx";
 
+extend({ OrbitControls });
 
- extend({OrbitControls})
+export default function Experience() {
+  const { camera, gl } = useThree();
 
-export default function Experience(){ 
-    const {camera, gl} = useThree();
-    
+  const Ref = useRef();
 
-    const Ref = useRef();
+  useFrame((state, delta) => {
+    let pixelRTION = state.gl.getPixelRatio();
+    if(pixelRTION <= 1){
+        state.gl.setPixelRatio(pixelRTION += 0.001) 
 
-    useFrame((state, delta)=> {
+    }
+    /** for camera rotation in circular motion
+ * 
+ *   let elapsedTime = state.clock.elapsedTime;
+
+        state.camera.position.x = Math.sin(elapsedTime) * 8
+        state.camera.position.z = Math.cos(elapsedTime) * 8
+        state.camera.lookAt(0, 0, 0);
         
-        Ref.current.rotation.y += delta 
-    })
-    return <>
-    {/* <OrbitControls a/> */}
-    <orbitControls args={[camera, gl.domElement]}/>
-    <directionalLight position={ [ 1, 2, 3 ] } intensity={2.5} />
-        <group ref={Ref}>
+ * **/
+
+    Ref.current.rotation.y += delta;
+  });
+  return (
+    <>
+      {/* <OrbitControls a/> */}
+      <orbitControls args={[camera, gl.domElement]} />
+      <directionalLight position={[1, 2, 3]} intensity={2.5} />
+      <group ref={Ref}>
         <mesh position-x={-2}>
-            <sphereGeometry/>
-            <meshStandardMaterial color="orange" />
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh  rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-            <boxGeometry/>
-            <meshStandardMaterial color="mediumpurple" />
+        <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
         </mesh>
-        </group>
-        <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow"/>
-        </mesh> 
+      </group>
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
 
-        <CustomObject/>
+      <CustomObject />
     </>
+  );
 }
--- 55-react-fiber-app/src/index.jsx
+++ 55-react-fiber-app/src/index.jsx
@@ -4,14 +4,23 @@ import ReactDOM from 'react-dom/client'
 import {Canvas} from '@react-three/fiber'
 import Experience from './Experience.jsx'
 
+import * as THREE from 'three'
+
 const root = ReactDOM.createRoot(document.querySelector('#root'))
+
 
 root.render(
     <Canvas
-        orthographic
+        // orthographic
+        dpr={0.01} // pixel ratio
+        gl={{ // changing renderer propertys 
+            antialias:  true,// default is true we can remove this
+            toneMapping: THREE.ACESFilmicToneMapping,
+        outputColorSpace: THREE.LinearSRGBColorSpace
+        }}
         camera={{
             fov: 45,
-            zoom: 100,  
+            // zoom: 100,  
             near: 0.1, // we can put all the camera setting in varia ble 
             far: 200,
             position: [3,2,6]
